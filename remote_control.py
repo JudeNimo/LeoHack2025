ece0daeae81143fa02c266309eb476fd64945d63
ada-85a5785b61 <ab2125@ic.ac.uk>
2025-11-08 19:22:14 +0000

    autonomous control added

--- remote_control.py
+++ remote_control.py
@@ -1,7 +1,5 @@
 import socket
-import time
-import keyboard  # pip install keyboard
-from datetime import datetime, timedelta
+from path_finder import sequence
 
 ARUINO_IP = "192.168.4.1"
 ARUINO_PORT = 8080
@@ -33,35 +31,45 @@ try:
     s.settimeout(5)
     s.connect((ARUINO_IP, ARUINO_PORT))
     print("Connected!")
-    print("Hold WASD/QE to move. Release to stop. Press ESC to exit.")
 
-    while True:
-        # Check for pressed keys
-        for key in key_to_command.keys():
-            if keyboard.is_pressed(key) and key not in pressed_keys:
-                pressed_keys.add(key)
-                cmd = key_to_command[key]
-                print(f"Sending command: {cmd}")
-                s.send(cmd.encode())
+    power = 7
+    duration = 1
 
-        # Check for released keyswsqeswswswqeqqeeqqwwsewww
-        keys_to_remove = []
-        for key in list(pressed_keys):
-            if not keyboard.is_pressed(key):
-                print(f"Key released: {key}")
-                s.send(stop_command.encode())
-                keys_to_remove.append(key)
-                
-        #remove released keys from the set
-        for key in keys_to_remove:
-            pressed_keys.remove(key)
+    name_to_command = {
+        'forward': f'f {power} {duration}', #forward
+        'backward': f'b {power} {duration}', #backward
+        'left': f'a {power} {duration}', #left
+        'right': f'd {power} {duration}', #right
+        'left_turn': f'l {power} {duration}', #left turn
+        'right_turn': f'r {power} {duration}', #right turn
+    }
 
-        # Check for exit key
-        if keyboard.is_pressed('esc'):
-            print("Exiting...")
-            break
+    def forward():
+        cmd = name_to_command['forward']
+        print(f"Sending command: {cmd}") 
+        s.send(cmd.encode())
 
-        time.sleep(0.05) # Small delay to avoid high CPU usage
+    def left():
+        cmd = name_to_command['left']
+        print(f"Sending command: {cmd}") 
+        s.send(cmd.encode())
+
+    def right():
+        cmd = name_to_command['right']
+        print(f"Sending command: {cmd}") 
+        s.send(cmd.encode())
+
+    def left_turn():
+        cmd = name_to_command['left_turn']
+        print(f"Sending command: {cmd}") 
+        s.send(cmd.encode())
+        
+    def right_turn():
+        cmd = name_to_command['right_turn']
+        print(f"Sending command: {cmd}") 
+        s.send(cmd.encode())
+
+    sequence(forward, left, right, left_turn, right_turn)
 
     s.close()
 
